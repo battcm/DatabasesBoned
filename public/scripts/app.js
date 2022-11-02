55ef5628243026e3d4a2af1e0b47ed2dced6a7ea
rhit-olingejj <olingejj@rose-hulman.edu>
2022-11-01 23:45:51 -0400

    front end of feature complete

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -34,7 +34,7 @@ rhit.main = function () {
 // Date picker only
 
 const userAction = async () => {
-	console.log('here');
+
 	const obj={ };
 	const response = await fetch(' http://localhost:8080/v1/select/RestName', {
 	  method: 'POST',
@@ -43,23 +43,80 @@ const userAction = async () => {
 		'Content-Type': 'application/json'
 	  }
 	});
-	const data=await response.json();
-	console.log(data);
-	for (let r of Object.values(data)) {
-		$('.dropdown-menu').append('<a class="dropdown-item" href="#">'+`${r.RestName}`+'<a>');
-			}
 
+			
 	// do something with myJson
 	if( response.status==200){
+		const data=await response.json();
+		console.log(data);
+		for (let r of Object.values(data)) {
+			$('#select1').append(`<option value="${r.RestName}">`+`${r.RestName}`+'</option>');
+				}
 
 	}else{
 		location.href="fail.html";
+	}
+}
+const userSub = async () => {
+	document.querySelector('#submit').onclick=(event)=>{
+		const sel=document.querySelector('#select1');
+		const day =document.querySelector('#datepicker');
+		const obj={day: `${day.value}`, rest:`${sel.value}` };
+		console.log(obj);
+		const response = await fetch(' http://localhost:8080/v1/select/app', {
+	  method: 'POST',
+	  body: JSON.stringify(obj),
+	  headers: {
+		'Content-Type': 'application/json'
+	  }
+	});
+	if( response.status==200){
+		const data=await response.json();
+		console.log(data);
+		let tab = 
+        `<tr>
+          <th>RestID</th>
+          <th>RestName</th>
+          <th>RestAddress</th>
+          <th>Food Name</th>
+		  <th>Food Calories</th>
+		  <th>Drink ID</th>
+		  <th>Drink Name</th>
+		  <th>Drink Brand</th>
+		  <th>Drink Price</th>
+		  <th>Food Date Recieved</th>
+		  <th>Quantity</th>
+		  <th>Storage Type</th>
+         </tr>`;
+		for (let r of Object.values(data)) {
+			tab += `<tr> 
+			<td>${r.RestID} </td>
+			<td>${r.RestName}</td>
+			<td>${r.RestAddress}</td> 
+			<td>${r.Name}</td> 
+			<td>${r.FoodCalories}</td> 
+			<td>${r.DrinkID}</td> 
+			<td>${r.DrinkName}</td> 
+			<td>${r.DrinkBrand}</td> 
+			<td>${r.DrinkPrice}</td> 
+			<td>${r.DateRecieved}</td> 
+			<td>${r.Quantity}</td> 
+			<td>${r.StorageType}</td> 
+		</tr>`;
+				}
+		document.querySelector('#restDay').innerHTML=(tab);
+
+	}else{
+		location.href="fail.html";
+	}
+
 	}
 }
 
 	
 
 	userAction();
+	userSub();
     
 	}
 
